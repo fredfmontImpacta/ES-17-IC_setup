4c0fe3af1dfd65069fc808df2c42e93e5ce068ff
Frederico Monteiro <frederico.monteiro@faculdadeimpacta.com.br>
2019-06-26 21:55:14 -0300

    Correção e nova função de multiplicação

--- teste.py
+++ teste.py
@@ -1,9 +1,14 @@
 import pytest
 from principal import somar
 from principal import subtrair
+from principal import multiplicar
 
 def test_somar():
     assert somar(2,3)==5
 
 def test_subtrair():
     assert subtrair(1,1)==0
+
+def test_multiplicar():
+    assert multiplicar(2,6)==12
+
